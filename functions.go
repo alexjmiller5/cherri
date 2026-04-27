a03f0ae60e0ac2ec524c741eccf4288c87d9a542
Brandon Jordan <brandonjordan124@gmail.com>
2026-04-27 19:26:25 -0400

    Replace empty dictionary variable in functions with constant for less actions

--- functions.go
+++ functions.go
@@ -135,8 +135,8 @@ func checkFunctionUsage(content string) {
 func generateFunctionsHeader() string {
 	var functionsHeader strings.Builder
 	functionsHeader.WriteString("if ShortcutInput {\n")
-	functionsHeader.WriteString("    @_cherri_empty_dictionary: dictionary\n")
-	functionsHeader.WriteString("    const _cherri_dictionary_type_name = typeOf(@_cherri_empty_dictionary)\n")
+	functionsHeader.WriteString("    const _cherri_empty_dictionary = {}\n")
+	functionsHeader.WriteString("    const _cherri_dictionary_type_name = typeOf(_cherri_empty_dictionary)\n")
 	functionsHeader.WriteString("    const _cherri_inputType = typeOf(ShortcutInput)\n")
 	functionsHeader.WriteString("    if _cherri_inputType == _cherri_dictionary_type_name {\n")
 	functionsHeader.WriteString("        const _cherri_input = getDictionary(ShortcutInput)\n")
